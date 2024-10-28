2a9aa8926f0719f0edebd654eefad79f1c0ea0f9
Wagner Rafael | AIExxplorer <132309825+AIExxplorer@users.noreply.github.com>
2024-10-28 01:04:59 +0000

    Challenge answer 6

--- 01_06b/1-6-sort-by-age.php
+++ 01_06b/1-6-sort-by-age.php
@@ -1,42 +1,65 @@
-
 <?php
 
-class Person {
+function check_capacity($capacity, $attendees = 0)
+{
+	// Verifica se o número de participantes é menor ou igual à capacidade
+	if ($attendees < $capacity) {
+		return 'Open';
+	} elseif ($attendees == $capacity) {
+		return 'Full';
+	} else {
+		return 'Over capacity';
+	}
+}
+
+function flip()
+{
+	// Gera um valor aleatório: cara ou coroa
+	return rand(0, 1) ? 'Heads' : 'Tails';
+}
+
+class Person
+{
 	private $name;
 	private $dob;
-	
-	function __construct($name, $dob) {
+
+	function __construct($name, $dob)
+	{
 		$this->name = $name;
-		$this->dob = $dob;	
+		$this->dob = $dob;
 	}
-	
-	public function get_name() {
+
+	public function get_name()
+	{
 		return $this->name;
 	}
-	
-	public function get_dob() {
+
+	public function get_dob()
+	{
 		return $this->dob;
 	}
-	
-	public function get_age() {
-		//Calculate age
-		$dob = new Datetime($this->dob);
-		$today = new Datetime(date('Y-m-d'));
+
+	public function get_age()
+	{
+		// Calcula a idade com base na data de nascimento
+		$dob = new DateTime($this->dob);
+		$today = new DateTime();
 		$age = $today->diff($dob);
-		
-		//Return Age in Years
 		return $age->y;
 	}
 }
 
-function print_array( $a ) {
+function compare_ages($p1, $p2)
+{
+	// Refatorada para ser usada com usort
+	return $p1->get_age() <=> $p2->get_age();
+}
+
+function print_array($a)
+{
 	echo '<pre>';
 	var_dump($a);
 	echo '</pre>';
-}
-
-function compare_ages( $p1, $p2) {
-	// Challenge: Refactor this function. 
 }
 
 $joe = new Person('Joe', '2010-10-20');
@@ -54,9 +77,29 @@ $jane = new Person('Jane', '2016-12-16');
 $john = new Person('John', '2007-04-30');
 $carl = new Person('Carl', '2010-03-25');
 
-$people = [ $joe, $phil, $erin, $alice, $bob, $carl, $jane, $john, $lou, $louis, $marie, $mike, $rob, $teresa];
+$people = [$joe, $phil, $erin, $alice, $bob, $carl, $jane, $john, $lou, $louis, $marie, $mike, $rob, $teresa];
 
-// Sort Here!
+// Ordena o array de pessoas por idade
+usort($people, 'compare_ages');
 
 // Print Results
-print_array( $people ); 
+print_array($people);
+
+?>
+
+<style>
+	.coin {
+		background: #999999;
+		color: #333333;
+		border-radius: 50%;
+		padding: 50px;
+		text-align: center;
+		font-size: 2rem;
+		font-weight: bold;
+		width: 50px;
+	}
+</style>
+
+<div class="coin">
+	<?php echo flip(); ?>
+</div>
