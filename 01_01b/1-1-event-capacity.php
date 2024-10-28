a5287db8d664169bd94f8ae76f79a1bf990a9a8c
Wagner Rafael | AIExxplorer <132309825+AIExxplorer@users.noreply.github.com>
2024-10-28 00:32:58 +0000

    Challenge answer 1

--- 01_01b/1-1-event-capacity.php
+++ 01_01b/1-1-event-capacity.php
@@ -1,18 +1,26 @@
 <?php
 
-function check_capacity( $capacity, $attendees = 0 ) {
-	// Challenge: define this function
+function check_capacity($capacity, $attendees = 0)
+{
+	// Verifica se o número de participantes é menor ou igual à capacidade
+	if ($attendees < $capacity) {
+		return 'Open';
+	} elseif ($attendees == $capacity) {
+		return 'Full';
+	} else {
+		return 'Over capacity';
+	}
 }
 
 // Array: key => [capacity, attendees];
-$venues = array( 
+$venues = array(
 	'Cantina' => [100, 20], //Droids not included.
-	'Dorsia' => [74, 74], 
-	'The Max'=> [98, 100], 
+	'Dorsia' => [74, 74],
+	'The Max' => [98, 100],
 	'MacLaren\'s' => [53, 127],
 	'The Banana Stand' => [2, 0],
 );
 
-foreach ( $venues as $name => $numbers ) {
+foreach ($venues as $name => $numbers) {
 	echo $name . ' - ' . check_capacity($numbers[0], $numbers[1]) . '<br/>';
 }
