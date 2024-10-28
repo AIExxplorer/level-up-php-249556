6ea4fadff2a8fe879ba9321a020366865d6f8c9f
Wagner Rafael | AIExxplorer <132309825+AIExxplorer@users.noreply.github.com>
2024-10-28 00:55:51 +0000

    Challenge answer 2

--- 01_02b/1-2-coin-flip.php
+++ 01_02b/1-2-coin-flip.php
@@ -1,3 +1,38 @@
+<?php
+
+function check_capacity($capacity, $attendees = 0)
+{
+	// Verifica se o número de participantes é menor ou igual à capacidade
+	if ($attendees < $capacity) {
+		return 'Open';
+	} elseif ($attendees == $capacity) {
+		return 'Full';
+	} else {
+		return 'Over capacity';
+	}
+}
+
+function flip()
+{
+	// Gera um valor aleatório: cara ou coroa
+	return rand(0, 1) ? 'Heads' : 'Tails';
+}
+
+// Array: key => [capacity, attendees];
+$venues = array(
+	'Cantina' => [100, 20], //Droids not included.
+	'Dorsia' => [74, 74],
+	'The Max' => [98, 100],
+	'MacLaren\'s' => [53, 127],
+	'The Banana Stand' => [2, 0],
+);
+
+foreach ($venues as $name => $numbers) {
+	echo $name . ' - ' . check_capacity($numbers[0], $numbers[1]) . '<br/>';
+}
+
+?>
+
 <style>
 	.coin {
 		background: #999999;
@@ -10,14 +45,6 @@
 		width: 50px;
 	}
 </style>
-
-<?php
-
-function flip() {
-	// Challenge: define this function
-}
-
-?>
 
 <div class="coin">
 	<?php echo flip(); ?>
